61ff04282c616646859192247ef9043a5d97bf1d
Will <wmchance@gmail.com>
2022-12-28 10:04:49 +0700

    Initialize Sqlite database

--- models.py
+++ models.py
@@ -2,13 +2,13 @@ from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
-engine = create_engine("sqlite:///inventory.db", echo=False)
+engine = create_engine('sqlite:///inventory.db', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
 class Brands(Base):
-    __tablename__ = "brands"
+    __tablename__ = 'brands'
 
     brand_id = Column(Integer, primary_key=True)
     brand_name = Column(String)
@@ -17,7 +17,7 @@ class Brands(Base):
         return f'<Brands(brand_id={self.brand_id}, brand_name={self.brand_name})>'
 
 class Product(Base):
-    __tablename__ = "products"
+    __tablename__ = 'products'
 
     product_id = Column(Integer, primary_key=True)
     product_name = Column(String)
@@ -28,3 +28,5 @@ class Product(Base):
 
     def __repr__(self): 
         return f'<Product(product_id={self.product_id}, product_name={self.product_name}, product_quantity={self.product_quantity}, product_price={self.product_price}, date_updated={self.date_updated}, brand_id={self.brand_id})>'
+
+
